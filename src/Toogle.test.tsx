9ef2a61baf330b01b849fe244bd57720f47aa353
Lukas <lukaskupczyk@users.noreply.github.com>
2022-12-23 19:45:25 +0100

    Add additional tests (#3)
    * Add provider tests
    
    * Add preferred theme tests
    
    * Bump version

--- src/Toogle.test.tsx
+++ src/Toogle.test.tsx
@@ -3,25 +3,27 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Toggle } from './Toggle';
 
-test('expect toggle not to be checked', async () => {
-    // ARRANGE
-    render(<Toggle />);
+describe('<Toggle />', () => {
+    test('expect toggle not to be checked', async () => {
+        // ARRANGE
+        render(<Toggle />);
 
-    // ASSERT
-    waitFor(() => {
-        expect(screen.getByRole('switch')).not.toBeChecked();
+        // ASSERT
+        waitFor(() => {
+            expect(screen.getByRole('switch')).not.toBeChecked();
+        });
     });
-});
 
-test('expect toggle to be checked', async () => {
-    // ARRANGE
-    render(<Toggle />);
+    test('expect toggle to be checked', async () => {
+        // ARRANGE
+        render(<Toggle />);
 
-    // ACT
-    await userEvent.click(screen.getByRole('switch'));
+        // ACT
+        await userEvent.click(screen.getByRole('switch'));
 
-    // ASSERT
-    waitFor(() => {
-        expect(screen.getByRole('switch')).toBeChecked();
+        // ASSERT
+        waitFor(() => {
+            expect(screen.getByRole('switch')).toBeChecked();
+        });
     });
 });
