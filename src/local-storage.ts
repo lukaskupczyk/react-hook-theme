f898e83bdd4f69779c417252e983a47b75f2e291
Lukas Kupczyk <lukas@kupczyk.net>
2022-12-25 23:24:21 +0100

    Cleanup

--- src/local-storage.ts
+++ src/local-storage.ts
@@ -1,15 +1,9 @@
 import { Theme } from './Context';
 
 export function storeTheme(theme: Theme) {
-    if (typeof window !== 'undefined') {
-        localStorage.setItem('rht-theme', theme);
-    }
+    localStorage.setItem('rht-theme', theme);
 }
 
 export function getStoredTheme() {
-    if (typeof window !== 'undefined') {
-        return localStorage.getItem('rht-theme') as Theme;
-    }
-
-    return null;
+    return localStorage.getItem('rht-theme') as Theme;
 }
